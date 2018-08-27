ff544aa1587d485d6810510a2c3b81c8df99af44
Gabriela Moreira <gabriela@magrathealabs.com>
2018-08-27 11:49:57 -0300

    More logic

--- firsttests.py
+++ firsttests.py
@@ -3,31 +3,47 @@ import pygame
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
-
-# This sets the margin between each cell
-MARGIN = 2
 
 N = 100
 n_dead = 700
+n_ants = 10
 
-# This sets the WIDTH and HEIGHT of each grid location
+MARGIN = 2
 WIDTH = 5
 HEIGHT = 5
 
-vision_field = 8
+vision_range = 1
+dead_ants = []
 
 class Ant():
     def __init__(self, size):
         self.x = int(random() * size)
         self.y = int(random() * size)
-        self.state = 0
+        self.carrying = False
     def move(self, size):
         self.x = max(min(self.x + randint(-1,1), size-1), 0)
         self.y = max(min(self.y + randint(-1,1), size-1), 0)
 
+        xmin = self.x-vision_range
+        xmax = self.x+vision_range+1
+        ymin = self.y-vision_range
+        ymax = self.y+vision_range+1
+
+        n_local = sum([sum(row[ymin : ymax]) for row in dead_ants[xmin : xmax]])
+        field_size = (1 + 2*vision_range)**2
+        if not self.carrying and dead_ants[self.x][self.y] == 1:
+            p = 1 - (n_local + 1) / field_size
+            if random() > p:
+                self.carrying = True
+                dead_ants[self.x, self.y] = 0
+        if self.carrying and dead_ants[self.x][self.y] == 0:
+            p = (n_local + 1) / field_size
+            if random() > p:
+                self.carrying = False
+                dead_ants[self.x, self.y] = 1
 
 def generate_grid(size=100):
     return [[0 for _ in range(size)] for _ in range(size)]
@@ -42,26 +58,21 @@ def spreads_itens(grid, n_itens):
         grid[i][j] = 1
     return grid
 
-test_grid = generate_grid(N)
 grid = generate_grid(N)
-dead_ants = spreads_itens(test_grid, n_dead)
+dead_ants = spreads_itens(generate_grid(N), n_dead)
 
-ant1 = Ant(N)
-grid[ant1.x][ant1.y] -= 1
-ant1.move(N)
-grid[ant1.x][ant1.y] += 1
+ants = [Ant(N) for _ in range(n_ants)]
 pygame.init()
 
 WINDOW_SIZE = [N * (WIDTH + MARGIN), N * (HEIGHT + MARGIN)]
 screen = pygame.display.set_mode(WINDOW_SIZE)
-pygame.display.set_caption("Array Backed Grid")
+pygame.display.set_caption("Ant Clustering")
 done = False
 clock = pygame.time.Clock()
 
 screen.fill(BLACK)
 
 while(not done):
-    # Draw the grid
     for row in range(N):
         for column in range(N):
             color = BLACK
@@ -76,19 +87,16 @@ while(not done):
                               WIDTH,
                               HEIGHT])
 
-    # Limit to 60 frames per second
     clock.tick(60)
-    grid[ant1.x][ant1.y] -= 1
-    ant1.move(N)
-    grid[ant1.x][ant1.y] += 1
+    for ant in ants:
+        grid[ant.x][ant.y] -= 1
+        ant.move(N)
+        grid[ant.x][ant.y] += 1
 
     for line in dead_ants:
         print(line)
     print('')
 
-    # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
 
-# Be IDLE friendly. If you forget this line, the program will 'hang'
-# on exit.
-#pygame.quit()
+pygame.quit()
